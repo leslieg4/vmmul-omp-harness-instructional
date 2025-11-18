b1ce72395882e89bdae8dc36169ac86bc6c6e4cb
Leslie Garcia <leslieg@x1004c4s3b0n0h0.chn.perlmutter.nersc.gov>
2025-11-17 22:44:43 -0800

    commiting the report.txt

---
+++ build/CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o: \
+ /global/homes/l/leslieg/vmmul-omp-harness-instructional/dgemv-basic.cpp \
+ /usr/include/stdc-predef.h
---
+++ build/CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o: \
+ /global/homes/l/leslieg/vmmul-omp-harness-instructional/dgemv-vectorized.cpp \
+ /usr/include/stdc-predef.h
